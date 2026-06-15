4564bad48f14fa422716a017454870b486d4f324
kachireddyrohitha-design <kachireddyrohitha@gmail.com>
2026-06-15 10:50:04 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/ewindsit/journal/Controller/JournalEntryController.java
@@ -0,0 +1,17 @@
+package com.ewindsit.journal.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JournalEntryController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+}
+
+
+
+
